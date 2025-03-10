de26fc67fae0391c07637b2ae31f57a3ba2357f3
최종현 <choejonghyeon@choejonghyeon-ui-MacBookPro-2.local>
2025-03-10 20:26:15 +0900

    credential test

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,16 @@ repositories {
 
 dependencies {
 	implementation("com.renovatio.toolkit:jpa-common:$jpaCommonVersion")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
